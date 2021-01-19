08b2fecd119166bf07b73a0ffabcae151b32006c
Martin G <martin_guga@live.com>
2021-01-19 13:37:20 +0000

    Add sizeCount
    Added a size counter for the search engine

--- ConstructionLine.CodingChallenge/SearchEngine.cs
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -1,23 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ConstructionLine.CodingChallenge
 {
     public class SearchEngine
     {
         private readonly List<Shirt> _shirts;
+        private readonly  List<SizeCount>_sizeCounts;
 
         public SearchEngine(List<Shirt> shirts)
         {
             _shirts = shirts;
-
-            // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
-
         }
 
 
         public SearchResults Search(SearchOptions options)
         {
-            // TODO: search logic goes here.
+            Parallel.ForEach(Size.All, size =>
+            {
+                _sizeCounts.Add(new SizeCount
+                {
+                    Count = _shirts.Count(s => s.Size == size
+                                               && (!options.Colors.Any() ||
+                                                   options.Colors.Select(c => c.Id).Contains(s.Color.Id))),
+                    Size = size
+                });
+            });
 
             return new SearchResults
             {
